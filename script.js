23861fd703e51872c20d60986c9c107d0f5bd18f
Oleg <dybovitskij@mail.ru>
2018-12-20 13:12:03 +0300

    fix bug

--- script.js
+++ script.js
@@ -1,48 +1,66 @@
 $(document).ready(function(e) {
-    function getColor(c) {
-        return $(c).attr('id');
+  function getColor(c) {
+    return $(c).attr("id");
+  }
+  let color;
+
+  $(".mini-button").click(function(e) {
+    e.preventDefault();
+    $(".mini-button").removeClass("active");
+    $(this).toggleClass("active");
+  });
+
+  $(".mini-button").on("click", function() {
+    color = getColor(this);
+  });
+
+  function onAdd(color) {
+    // console.log('color', color);
+  }
+  $(".button").on("click", function() {
+    onAdd(color);
+    let toAdd = $("input[name=ListItem]").val();
+
+    $("input").keyup(function() {
+      str = $(this).val();
+      str = str.replace(/\s/g, "");
+      $(this).val(str);
+    });
+
+    if (!toAdd) {
+      return false;
+    } else {
+      $("ul").prepend(
+        "<li>" +
+          "<div class = 'check__button color1 " +
+          color +
+          "'>" +
+          " <input class='status' type='checkbox'/>" +
+          " <p class='text-item'>" +
+          toAdd +
+          "</p>" +
+          "</div>" +
+          "</li>" +
+          "<br>"
+      );
+      $("#form-id")[0].reset(); // clean input
     }
-    let color;
-    $(".mini-button").on("click", function() {
-        color = getColor(this);
+  });
+
+  $(document).on("click", "input.status", function() {
+    $(".check__button").on("click", "input.status", function() {
+      let colors = ["f16d69", "f279a2", "9170cb", "5eb3f6", "67d7e5", "ffe083"];
+      $(this)
+        .parent()
+        .css("background", "#" + getNumber(colors)); // random color
     });
 
-    function onAdd(color) {
-        // console.log('color', color);
+    function getNumber(list) {
+      return list[Math.floor(Math.random() * list.length)];
     }
-    $(".button").on("click", function() {
-        onAdd(color);
-
-        let toAdd = $("input[name=ListItem]").val();
-
-        $('input').keyup(function() {
-            str = $(this).val()
-            str = str.replace(/\s/g, '')
-            $(this).val(str)
-        });
-
-        if (!toAdd) {
-            return false;
-        }
-        if (color == undefined) {
-            return false;
-        } else {
-            $('ul').prepend("<li>" + "<div class = 'check__button " + color + "'>" + " <input class='status' type='checkbox'/>" + " <p class='text-item'>" + toAdd + "</p>" + "</div>" + "</li>" + "<br>");
-            $("#form-id")[0].reset(); // clean input
-            color = null;
-
-        }
-    });
-    $(document).on("click", "input.status", function() {
-        $(".check__button").on("click", "input.status", function() {
-            let colors = ["f16d69", "f279a2", "9170cb", "5eb3f6", "67d7e5", "ffe083"];
-            $(this).parent().css("background", "#" + getNumber(colors)); // random color
-        });
-
-        function getNumber(list) {
-            return list[Math.floor(Math.random() * list.length)];
-        }
-        $(this).parent().find("p").toggleClass("complete");
-    });
-
+    $(this)
+      .parent()
+      .find("p")
+      .toggleClass("complete");
+  });
 });
